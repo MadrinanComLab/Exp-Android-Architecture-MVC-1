d5716a57d188da522aad2e0268275af4f210f154
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-03 01:49:45 +0700

    Values in strings.xml was changed

---
+++ app/src/main/java/com/madrinancomlab/exp_android_mvc_architecture/MainActivity.java
@@ -0,0 +1,14 @@
+package com.madrinancomlab.exp_android_mvc_architecture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
